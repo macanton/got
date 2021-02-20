17fe1ffb615390ca586c0493d5ac9b2e0ed5e578
Anton Machkasov <anton.machkasov@gmail.com>
2021-02-20 10:22:00 +0200

    Update goreleaser version, go version and darwin arm64 build

---
+++ pkg/git/utils_test.go
@@ -0,0 +1,61 @@
+package git
+
+import (
+	"testing"
+)
+
+func TestGenerateBranchName_WithoutJiraIssueKeys(t *testing.T) {
+	branchName, err := GenerateBranchName([]string{}, "Test string  data")
+
+	if err != nil {
+		t.Errorf("GenerateBranchName without Jira issue keys returned error %+v", err.Error())
+	}
+
+	expectedBranchName := "test_string__data"
+	if branchName != expectedBranchName {
+		t.Errorf("GenerateBranchName without Jira issue keys returned %+v, want %+v", branchName, expectedBranchName)
+	}
+}
+
+func TestGenerateBranchName_WithSeveralJiraIssueKeys(t *testing.T) {
+	branchName, err := GenerateBranchName([]string{"PC-1234", "PC-345"}, "Test string  data")
+
+	if err != nil {
+		t.Errorf("GenerateBranchName without Jira issue keys returned error %+v", err.Error())
+	}
+
+	expectedBranchName := "PC-1234/PC-345/test_string__data"
+	if branchName != expectedBranchName {
+		t.Errorf("GenerateBranchName without Jira issue keys returned %+v, want %+v", branchName, expectedBranchName)
+	}
+}
+
+func TestPrependIssueKeysToBranchName_WithoutJiraIssueKeys(t *testing.T) {
+	branchName, err := PrependIssueKeysToBranchName([]string{}, "test_branch_name")
+
+	if err != nil {
+		t.Errorf("GenerateBranchName without Jira issue keys returned error %+v", err.Error())
+	}
+
+	expectedBranchName := "test_branch_name"
+	if branchName != expectedBranchName {
+		t.Errorf("GenerateBranchName without Jira issue keys returned %+v, want %+v", branchName, expectedBranchName)
+	}
+}
+
+func TestPrependIssueKeysToBranchName_WithJiraIssueKeys(t *testing.T) {
+	branchName, err := PrependIssueKeysToBranchName([]string{"PC-123", "PC-345"}, "test_branch_name")
+
+	if err != nil {
+		t.Errorf("PrependIssueKeysToBranchName without Jira issue keys returned error %+v", err.Error())
+	}
+
+	expectedBranchName := "PC-123/PC-345/test_branch_name"
+	if branchName != expectedBranchName {
+		t.Errorf(
+			"PrependIssueKeysToBranchName without Jira issue keys returned %+v, want %+v",
+			branchName,
+			expectedBranchName,
+		)
+	}
+}
